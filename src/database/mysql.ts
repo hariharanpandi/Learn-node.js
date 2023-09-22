28a537c9559f6ded6a7601ce5a12a832e1792abf
Hariharan <hariharan.p@aaludra.com>
2023-09-22 17:28:47 +0530

    Docker-mysql port change

--- src/database/mysql.ts
+++ src/database/mysql.ts
@@ -3,14 +3,17 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const port: number = Number(`${process.env.SQL_PORT}`);
+
 const connectToDB = new Sequelize(
-    `${process.env.DB_NAME}`,       
-    `${process.env.DB_USER}`,      
-    `${process.env.DB_PASSWORD}`,   
-    {
-      host: `${process.env.DB_HOST}`,  
-      dialect: 'mysql',
-    }
-  );
+  `${process.env.DB_NAME}`,
+  `${process.env.DB_USER}`,
+  `${process.env.DB_PASSWORD}`,
+  {
+    host: `${process.env.DB_HOST}`,
+    dialect: 'mysql',
+    port: port
+  }
+);
 
 export default connectToDB;
